896dbfd5ee2f341db248f3cf2f2718f0088df0c6
Roman3312 <35339347+Roman3312@users.noreply.github.com>
2018-01-11 18:41:02 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer(function (request,responce) {
+    console.log(request.method,request.url);
+    if (request.url == '/stylemove.css'){
+        const css = fs.readFileSync('stylemove.css','utf8');
+        responce.end(css);
+    } else if (request.url == '/scriptmove.js'){
+        const script = fs.readFileSync('scriptmove.js','utf8');
+        responce.end(script);
+    } else if (request.url == '/favicon.ico'){
+        const favicon = fs.readFileSync('favicon.ico');
+        responce.end(favicon);
+    } else {
+        const html = fs.readFileSync('indexmove.html','utf8');
+        responce.end(html);
+    }
+
+
+
+    const html = fs.readFileSync('indexmove.html','utf8');
+   responce.end(html);
+});
+
+
+server.listen(3000);
+console.log('Server started');
